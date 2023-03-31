1f3db54909bbb0a70380f327d6dd189665467994
SimoLomba53 <simolomba253@gmail.com>
2023-03-31 15:02:06 +0200

    aggiunta seeder

---
+++ database/seeders/SongSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use App\Models\Song;
+
+class SongSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
